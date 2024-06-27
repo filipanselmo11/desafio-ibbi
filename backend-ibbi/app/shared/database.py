e7c855c85dbc2bb51c1957111476e6474afd5a59
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-06-27 18:08:23 -0400

    feat: subindo docker file do backend

--- backend-ibbi/app/shared/database.py
+++ backend-ibbi/app/shared/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATA_BASE_URL = "postgresql+psycopg2://admin:senha123@localhost/ibbi_db"
-engine = create_engine(SQLALCHEMY_DATA_BASE_URL)
+DATA_BASE_URL = "postgresql+psycopg2://admin:senha123@localhost/ibbi_db"
+engine = create_engine(DATA_BASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 Base = declarative_base()
